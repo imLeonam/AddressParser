ddf091c90ecd64fd5fa3be679c7a41be731e0e7f
Leonam <leonammrib@gmail.com>
2021-12-13 12:03:03 -0300

    add - toUpperCase

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 const { parse } = require('csv-parse/sync');
 const fs = require('fs');
 
@@ -9,6 +10,9 @@ const records = parse(input, {
   skip_empty_lines: true
 });
 
+records.map(record => {
+    record.descricao = record.descricao.toLocaleUpperCase();
+});
 
 //CEP
 let cepCount = 0;
@@ -24,10 +28,14 @@ records.map(record => {
 
 })
 
+let emailCount = 0;
 //email
 records.map(record => {
   let match = (/([a-z]+\.?[a-z]+@[a-z]+(\.[a-z]{2,3}){1,2})\;?/g).exec(record.email);
-  if (!match) return;
+  if (!match) {
+    emailCount++;
+    return
+  };
 
   record.email = match[1];
   record.email = record.email.replace(match[3], '');
@@ -37,27 +45,32 @@ records.map(record => {
 
 //Rua-Numero-Bairro
 records.map(record => {
-  let match = (/((RUA|Rua|R\.|AVENIDA|Avenida|AV\.|Av\.|TRAVESSA|Travessa|TRAV\.|Trav\.|Estr\.|estr\.|Estrada|BR) (([A-zÁ-úã_\s\.])+))[, ]+(\d+)\s?\-?(CEP\d+)?(\d+ )?(\/?[\/\da-zDA-Zúãâôíê\ ]+)?/g).exec(record.endereco);
+  let match = (/((RUA|Rua|R\.|AVENIDA|Avenida|AV\.|Av\.|TRAVESSA|Travessa|TRAV\.|tv\.|Tv\.|Trav\.|Estr\.|estr\.|Estrada|BR) (([A-zÁ-úã_\s\.])+))[, ]+(\d+)\s?\-?(CEP\d+)?(\d+ )?(\/?[\/\da-zDA-Zúãâôíê\ ]+)?/g).exec(record.endereco);
   if (!match) return;
 
-  record.rua = match[1];
+  record.rua = match[1].toLocaleUpperCase();
   record.numero = match[5];
   record.bairro = match[8];
 
   console.log(record);
 })
 
+let ufCount = 0;
 //UF-cidade
 records.map(record => {
   let match = (/((\W)(RO|AC|AM|RR|PA|AP|TO|MA|PI|CE|RN|PB|PE|AL|SE|BA|MG|ES|RJ|SP|PR|SC|RS|MS|MT|GO|DF{2})\b(\W))/g).exec(record.endereco);
-  if (!match) return;
-
+  if (!match) {
+    ufCount++;
+    return;
+  };
   record.UF = match[1].trim();
   record.cidade = record.endereco.replace(match[3, 4], '');
 
   console.log(record);
 })
 
+let cidadeCountNull = 0;
+let cidadeCount = 0;
 //cidade
 records.map(record => {
   let match = (/(([A-zÃ-õÂ-û]+ ?[A-zÃ-õÂ-û]+ ?\s[A-zÃ-õÂ-û]+ ?[A-zÃ-õÂ-û]+) (\-|,) ([A-Z]{2})?)( |,)/g).exec(record.cidade);
@@ -69,10 +82,14 @@ records.map(record => {
   console.log(record);
 })
 
+let siteCount = 0;
 //Site
 records.map(record => {
   let match = (/(http:\/\/(\w+\.[A-z]+\.(\w+)?\.(\w+)?\/?))/g).exec(record.site);
-  if (!match) return;
+  if (!match) {
+    siteCount++;
+    return
+  };
 
   record.site = match[1];
 
@@ -85,6 +102,17 @@ records.map(record => {
   csv += `${record.nome || ''};${record.email || ''};${record.telefone || ''};${record.site || ''};${record.cidade || ''};${record.rua || ''};${record.numero || ''};${record.bairro || ''};${record.cep || ''};${record.UF || ''};\n`;
 })
 
-console.log("cepCount: " + cepCount);
+var calculoCidadeNotFound = 0;
+if (cidadeCount > cidadeCountNull) {
+  calculoCidadeNotFound = cidadeCount - cidadeCountNull;
+}else {
+  calculoCidadeNotFound = cidadeCountNull - cidadeCount;
+}
+
+// console.log("cepCount: " + cepCount + "\n" + 
+// "siteCount: " + siteCount + "\n" + 
+// "CidadeCount: " + cidadeCount + ' encontradas, ' + calculoCidadeNotFound + ' nao encontradas' + "\n" + 
+// "UFCount: " + ufCount + "\n" + 
+// "EmailCount: " + emailCount);
 
 fs.writeFileSync('OUTPUT.csv', csv, { encoding: 'latin1' });
