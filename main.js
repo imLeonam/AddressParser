a686a8dc50dfd7b0fc84abe5f6c63dc07faf4b27
Leonam <leonammrib@gmail.com>
2021-12-16 13:37:54 -0300

    chore: parse address

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 const { parse } = require('csv-parse/sync');
 const fs = require('fs');
+const { resourceLimits } = require('worker_threads');
 
 const input = fs.readFileSync('TABELA.CSV', 'latin1');
 
@@ -11,108 +12,94 @@ const records = parse(input, {
 });
 
 records.map(record => {
-    record.descricao = record.descricao.toLocaleUpperCase();
+  record.descricao = record.descricao.toLocaleUpperCase();
 });
 
 //CEP
-let cepCount = 0;
 records.map(record => {
-  let match = (/(C[Ee][Pp]:?)? (\d{5}[- ]\d{3})/g).exec(record.endereco);
-  if (!match) {
-    cepCount++;
-    return;
-  };
+  let match = (/(C[Ee][Pp]:?)? (\d{5}[- ]?\d{3})/g).exec(record.endereco);
+  if (!match) return;
 
   record.cep = match[2];
-  record.endereco = record.endereco.replace(match[0], '');
 
 })
 
-let emailCount = 0;
 //email
 records.map(record => {
   let match = (/([a-z]+\.?[a-z]+@[a-z]+(\.[a-z]{2,3}){1,2})\;?/g).exec(record.email);
-  if (!match) {
-    emailCount++;
-    return
-  };
+  if (!match) return;
 
   record.email = match[1];
   record.email = record.email.replace(match[3], '');
 
   console.log(record);
-})
+});
 
-//Rua-Numero-Bairro
+//Rua
 records.map(record => {
-  let match = (/((RUA|Rua|R\.|AVENIDA|Avenida|AV\.|Av\.|TRAVESSA|Travessa|TRAV\.|tv\.|Tv\.|Trav\.|Estr\.|estr\.|Estrada|BR) (([A-zÁ-úã_\s\.])+))[, ]+(\d+)\s?\-?(CEP\d+)?(\d+ )?(\/?[\/\da-zDA-Zúãâôíê\ ]+)?/g).exec(record.endereco);
+  let match = (/((RUA|Rua|R\.|AVENIDA|Avenida|AV\.|Av\.|TRAVESSA|Travessa|TRAV\.|tv\.|Tv\.|Trav\.|Estr\.|estr\.|Estrada|BR)\W*([A-zÁ-úã_\s\d?\.]+))\W*?/g).exec(record.endereco);
   if (!match) return;
 
-  record.rua = match[1].toLocaleUpperCase();
-  record.numero = match[5];
-  record.bairro = match[8];
+  record.rua = match[1].replace(/\W*((Ap\.?) [A-zÁ-úã_\s\d?\.]+)/g, '');
+
 
   console.log(record);
-})
+});
 
-let ufCount = 0;
+//complemento
+records.map(record => {
+  let match = (/\W*((Ap\.?)[A-zÁ-úã_\s\d?\.]+)/g).exec(record.endereco);
+  if (!match) return;
+  record.compl = match[1];
+});
+
+//Numero
+records.map(record => {
+  let match = (/\W+(\d+|s\/n)\W*/g).exec(record.endereco);
+  if (!match) return;
+
+  record.numero = match[1];
+  console.log(record);
+});
+
+//Bairro
+records.map(record => {
+  let match = (/(\W*?\d+\W?|s\/n)\W*(Bairro:? ?)?([A-zÁ-úã_\s.]+)\W*?/g).exec(record.endereco);
+  if (!match) return;
+
+  record.bairro = match[3];
+  console.log(record);
+
+
+  console.log(record);
+});
+
+
 //UF-cidade
 records.map(record => {
-  let match = (/((\W)(RO|AC|AM|RR|PA|AP|TO|MA|PI|CE|RN|PB|PE|AL|SE|BA|MG|ES|RJ|SP|PR|SC|RS|MS|MT|GO|DF{2})\b(\W))/g).exec(record.endereco);
-  if (!match) {
-    ufCount++;
-    return;
-  };
-  record.UF = match[1].trim();
-  record.cidade = record.endereco.replace(match[3, 4], '');
-
-  console.log(record);
-})
-
-let cidadeCountNull = 0;
-let cidadeCount = 0;
-//cidade
-records.map(record => {
-  let match = (/(([A-zÃ-õÂ-û]+ ?[A-zÃ-õÂ-û]+ ?\s[A-zÃ-õÂ-û]+ ?[A-zÃ-õÂ-û]+) (\-|,) ([A-Z]{2})?)( |,)/g).exec(record.cidade);
+  let match = (/(Cidade: ?)?\W*([A-zÁ-úã_\s\d]+)\W*((RO|AC|AM|RR|PA|AP|TO|MA|PI|CE|RN|PB|PE|AL|SE|BA|MG|ES|RJ|SP|PR|SC|RS|MS|MT|GO|DF{2})\W*)/).exec(record.endereco);
   if (!match) return;
 
-  record.cidade = match[2];
-  record.cidade = record.cidade.replace(match[4], '');
+  record.uf = match[4];
+  record.cidade = match[2].replace(/\W*[A-Z]{2}/g, '').trim();
 
   console.log(record);
-})
+});
 
-let siteCount = 0;
 //Site
 records.map(record => {
   let match = (/(http:\/\/(\w+\.[A-z]+\.(\w+)?\.(\w+)?\/?))/g).exec(record.site);
-  if (!match) {
-    siteCount++;
-    return
-  };
+  if (!match) return;
 
   record.site = match[1];
 
   console.log(record);
-})
+});
 
 
 let csv = "";
 records.map(record => {
   csv += `${record.nome || ''};${record.email || ''};${record.telefone || ''};${record.site || ''};${record.cidade || ''};${record.rua || ''};${record.numero || ''};${record.bairro || ''};${record.cep || ''};${record.UF || ''};\n`;
-})
-
-var calculoCidadeNotFound = 0;
-if (cidadeCount > cidadeCountNull) {
-  calculoCidadeNotFound = cidadeCount - cidadeCountNull;
-}else {
-  calculoCidadeNotFound = cidadeCountNull - cidadeCount;
-}
-
-// console.log("cepCount: " + cepCount + "\n" + 
-// "siteCount: " + siteCount + "\n" + 
-// "CidadeCount: " + cidadeCount + ' encontradas, ' + calculoCidadeNotFound + ' nao encontradas' + "\n" + 
-// "UFCount: " + ufCount + "\n" + 
-// "EmailCount: " + emailCount);
+});
 
 fs.writeFileSync('OUTPUT.csv', csv, { encoding: 'latin1' });
